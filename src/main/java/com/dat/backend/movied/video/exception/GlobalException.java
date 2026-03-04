f4b9d3b3c72ededf6f7ce2a9f4ac59e0780b6047
Đạt <datnguyen101004@gmail.com>
2026-03-04 15:16:11 +0700

    feat: handle exception

---
+++ src/main/java/com/dat/backend/movied/video/exception/GlobalException.java
@@ -0,0 +1,23 @@
+package com.dat.backend.movied.video.exception;
+
+import com.dat.backend.movied.video.dto.ResponseApi;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class GlobalException {
+
+    @ExceptionHandler(VideoUploadException.class)
+    @ResponseStatus(HttpStatus.REQUEST_TIMEOUT)
+    public ResponseApi<?> videoUploadException(VideoUploadException e) {
+        return ResponseApi.fail(HttpStatus.REQUEST_TIMEOUT.value(), e.getMessage());
+    }
+
+    @ExceptionHandler(Exception.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ResponseApi<?> exception(Exception e) {
+        return ResponseApi.fail(HttpStatus.BAD_REQUEST.value(), e.getMessage());
+    }
+}
---
+++ src/main/java/com/dat/backend/movied/video/exception/VideoUploadException.java
@@ -0,0 +1,7 @@
+package com.dat.backend.movied.video.exception;
+
+public class VideoUploadException extends RuntimeException {
+    public VideoUploadException(String message) {
+        super(message);
+    }
+}

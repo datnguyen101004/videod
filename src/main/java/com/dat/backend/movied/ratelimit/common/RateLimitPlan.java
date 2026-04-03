d512a6eb67f234b6aaf746058f76534d8243f529
Đạt <datnguyen101004@gmail.com>
2026-04-03 14:52:44 +0700

    fix: increase duration for ensure not refill in test case

--- src/main/java/com/dat/backend/movied/ratelimit/common/RateLimitPlan.java
+++ src/main/java/com/dat/backend/movied/ratelimit/common/RateLimitPlan.java
@@ -7,9 +7,9 @@ import java.time.Duration;
 @Getter
 public enum RateLimitPlan {
 
-    FREE(5, 1, Duration.ofMillis(10)),
-    PREMIUM(20, 4, Duration.ofMillis(10)),
-    MAX_PREMIUM(1000, 10, Duration.ofMillis(10));
+    FREE(5, 1, Duration.ofMillis(30)),
+    PREMIUM(20, 4, Duration.ofMillis(30)),
+    MAX_PREMIUM(1000, 10, Duration.ofMillis(30));
 
     private final Long capacity;
     private final Long refillTokens;
--- src/test/java/com/dat/backend/movied/video/integration_test/VideoServiceTest.java
+++
@@ -1,4 +0,0 @@
-package com.dat.backend.movied.video.integration_test;
-
-public class VideoServiceTest {
-}
